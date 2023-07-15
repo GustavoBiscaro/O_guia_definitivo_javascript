df66b065b65493c2af09adc17f0ec55267e9f764
Gustavo Miguel Oliveira Biscaro <gustavo.biscaro@gmail.com>
2023-07-15 11:47:48 -0300

    Uploads

--- CHAPTER ONE - Introdução ao Javascript/js/capt1.js
+++ CHAPTER ONE - Introdução ao Javascript/js/capt1.js
@@ -16,7 +16,7 @@ console.log(primes.length - 1)
 console.log(primes[0] + primes[2])
 
 // TIPOS DE VALORES
-var numero  = 1;
+var numero = 1;
 var numeroInteiro = 0.02;
 var string = "hello world";
 var stringAspasSimples = 'JavaScript';
@@ -34,8 +34,8 @@ var book = {
 
 // Acesse as propriedades de um objeto com um ponto (.) ou []: 
 console.log("Acessando propriedades: " + "\n" +
-book.topic + "\n" +
-book["fat"]);
+    book.topic + "\n" +
+    book["fat"]);
 
 // Criando novas propriedades
 book.author = "Flanagan"; // nova propriedade "author"
@@ -48,7 +48,7 @@ var primos = [2, 3, 5, 7]; // Um array de 4 valores, delimitados com [ e ]
 console.log(primos[0]); // retorna o primeiro elemento [indice 0] do array
 console.log(primos.length) // retorna a quantidade de elementos
 
-var ultimo = primos[primos.length-1]; /* trabalha o cálculo da posição do elemento, neste caso, -1 e não o valor */
+var ultimo = primos[primos.length - 1]; /* trabalha o cálculo da posição do elemento, neste caso, -1 e não o valor */
 console.log(ultimo);
 
 /*  Explicando detalhadamente
@@ -72,13 +72,13 @@ console.log(empty.length);
 // Arrays podem conter outros arrays e objetos
 
 var points = [   // Array com 2 elementos; cada elemento é um objeto
-    {x:0, y:0},
-    {x:1, y:1}
+    { x: 0, y: 0 },
+    { x: 1, y: 1 }
 ];
 
 var data = {
-    trial1: [[1,2], [3,4]],  // Um objeto com duas propriedades, o valor de cada propriedade é um array e os elementos dos arrays são arrays.
-    trial2: [[2,3], [4,5]]
+    trial1: [[1, 2], [3, 4]],  // Um objeto com duas propriedades, o valor de cada propriedade é um array e os elementos dos arrays são arrays.
+    trial2: [[2, 3], [4, 5]]
 };
 
 // Operadores aritméticos
@@ -86,10 +86,10 @@ var a, b;
 a = 10;
 b = 2;
 
-console.log("Soma: " +parseInt(a+b));
-console.log("Subtração: " +parseInt(a-b));
-console.log("Multiplicação: " +parseInt(a*b));
-console.log("Divisão: " +parseInt(a/b));
+console.log("Soma: " + parseInt(a + b));
+console.log("Subtração: " + parseInt(a - b));
+console.log("Multiplicação: " + parseInt(a * b));
+console.log("Divisão: " + parseInt(a / b));
 console.log("Concatenando: " + "10" + "2");
 
 // Operadores de forma abreviada
@@ -115,9 +115,9 @@ x >= y; //maior ou igual que
 // "two" > "three"; verdadeiro, tw é alfabeticamente maior do que th
 
 // operadores lógicos
-console.log((2 == 2) && (5  > 2)) //Operador E, ambos tem que serem verdadeiros
+console.log((2 == 2) && (5 > 2)) //Operador E, ambos tem que serem verdadeiros
 
-console.log((3 > 2) || (2^3 > 10)) //Operador OU, uma das duas condições precisam serem verdadeiras
+console.log((3 > 2) || (2 ^ 3 > 10)) //Operador OU, uma das duas condições precisam serem verdadeiras
 
 var comparando = !(x > 2 || y > 5) // false
 console.log(comparando) // inverte o valor, se era falso, se tornou verdadeiro
@@ -129,14 +129,15 @@ console.log(!(x > 2 || y > 5)); // alternativa de negação
 console.log("A" > "C") // false
 
 // Primeira vista de funções
+
 function plus1(x) {  // Define uma função chamada plus1, com o parâmetro x
-    return x+1;     // retorna um valor uma unidade maior do que foi passado                    //
+    return x + 1;     // retorna um valor uma unidade maior do que foi passado                    //
 }                   // As funções são incluídas entre chaves
 
 console.log(plus1(y)); // => 4: y é 3, portanto, essa chamada retorna 3+1
 
-var square = function(x) { // As funções são valores e podem ser atribuídos a variáveis
-    return x*x; // Calcula o valor da função
+var square = function (x) { // As funções são valores e podem ser atribuídos a variáveis
+    return x * x; // Calcula o valor da função
 }; // Um ponto e vírgula marca o fim da atribuição
 
 console.log(square(plus1(y)));  // plus1(y)= 4 , logo square(quadrado) de 4 é 16.
@@ -144,4 +145,49 @@ console.log(square(plus1(y)));  // plus1(y)= 4 , logo square(quadrado) de 4 é 1
 // Quando combinamos funções com objetos, obtemos métodos
 
 // Quando funções recebem as propriedades de um objeto, as 
-// chamamos de "métodos". Todos os objetos de javascript têm métodos
+// chamamos de "métodos". Todos os objetos de javascript têm métodos:
+
+var a = []; // Cria um array vazio
+a.push(1, 2, 3) // O método push() adiciona elementos em um array
+a.reverse(); // Outro método: inverte a ordem dos elementos
+console.log(a); // Exibe o array atualizado
+
+// Também podemos definir nossos próprios métodos. A palavra chave "this" se refere ao
+// objeto no qual o método é definido: neste caso, o array de pontos anterior.
+
+points.dist = function () { // Defome um método para calcular a distância entre pontos
+    var p1 = this[0]; // Primeiro elemento do array que chamamos
+    var p2 = this[1]; // Segundo elemento do objeto "this"
+    var a = p2.x - p1.x; // Diferença em coordenadas X
+    var b = p2.y - p2.y; // Diferença em coordenadas y
+    return Math.sqrt(a * a + b * b); // Teorema de Pitágoras; Math.sqrt() calcula a raiz quadrada
+};
+
+console.log(points.dist());
+
+
+// As instruções JavaScript incluem condicionais e laços que usam a sintaxe das linguagens C, C++, Java, entre outras
+
+function valorAbsoluto(x) { // Uma função para calcular o valor absoluto
+    if (x >= 0) { // A instrução If... 
+        return x; // Executa o código, se a comparação for verdadeira
+    } // Fim da cláusula If
+
+    else { // A cláusula opcional else executa seu código se 
+        return -x; // a comparação for falsa
+    } // Chaves são opcionais quando há 1 instrução por cláusula.
+} // Observe as instruções return aninhadas dentro do if/else
+
+console.log(valorAbsoluto(-5))
+
+function fatorial(n) { // Uma função para calcular fatoriais
+    var produto = 1; // Começa com o produto de 1
+    while(n > 1) { // Repete as instruções que estão em {}, enquanto
+                    // expressão em () for verdadeira
+        produto *= n; // Atalho para produto = produto * n;
+        n--; // Atalho para n = n - 1
+    } // Fim do laço
+    return produto; // retorna o produto
+}
+
+console.log(fatorial(3)); // 3 = 3.2.1 = 6
